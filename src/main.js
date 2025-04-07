86047b5b13c4d41e923a56274be0252b6c13eef0
Poku <poku1010@gmail.com>
2025-04-07 09:00:20 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,1 @@
+// logic is inline in index.html
---
+++ src/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'birdaway-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
